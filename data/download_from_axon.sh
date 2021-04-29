889e889b7de09fe00bcc3d68e0809382b0bb4d89
Taylor <tahlor@gmail.com>
2021-04-29 17:23:13 -0600

    update data scripts

--- data/download_from_axon.sh
+++ data/download_from_axon.sh
@@ -1,5 +1,5 @@
-ROOT="$( cd -- "$(dirname $(dirname "$0"))" >/dev/null 2>&1 ; pwd -P )"
-
+ROOT="$(dirname $( cd -- "$(dirname "$0"))" >/dev/null 2>&1 ; pwd -P ))"
+echo $ROOT
 wget axon.cs.byu.edu/media/data.zip -O $ROOT/data.zip --show-progress
 
 # use unzip -o to overwrite without prompting
