c49fdd1b9343e72e277c85a0fca7e3542077cf1c
bjamin5 <ben.graff645@gmail.com>
2021-05-06 17:46:55 -0600

    Outputs lists of tensors, needs to be stacked tensors

--- data/sen_loader.py
+++ data/sen_loader.py
@@ -5,6 +5,7 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 import pandas
+import string
 import numpy as np
 import matplotlib.pyplot as plt
 from torch import float32
@@ -51,21 +52,27 @@ class SentencesDataset(Dataset):
 
         self.text = open_file(file)
         self.len = len(self.text)
+        random.seed = random.random()
+
 
     def __len__(self):
         return self.len
 
-    def __getitem__(self, item):
-        """Get's random sentences of size 32 chars and samples EMNIST for the corresponding character images"""
+    def __getitem__(self, idx):
+        """Get's random sentences of size 32 chars and samples EMNIST for the corresponding character images
+        Returns a list of an image for every character in the sentence. """
         a = random.randrange(0, self.len - 32)
         b = a + 32
         sentence = self.text[a:b]
         x = list()
         for char in sentence:
-            pdb.set_trace()
-            test = self.emnist_loaded.sample(char)
-            x.append(self.emnist_loaded.sample(char))
-        return
+            #pdb.set_trace()
+            x.append((self.emnist_loaded.sample(char), char)) # list of tuples of images [0], with labels [1]
+        #pdb.set_trace()
+        #for data in x:
+        #    print(data[1])
+        #pdb.set_trace()
+        return x
 
 
 class EmnistSampler:
@@ -91,12 +98,6 @@ class EmnistSampler:
             char = num_to_letter(x[1])
             if char in letters:
                 letters[char].append(x[0])
-            elif char == ' ':
-                pdb.set_trace()
-                space_tensor = np.full((1, 28, 28), dtype=float32, fill_value=-0.4242)
-                space_tensor = torch.from_numpy(space_tensor)
-                letters[char] = list()
-                letters[char].append(space_tensor)
             else:
                 letters[char] = list()
                 letters[char].append(x[0])
@@ -107,31 +108,34 @@ class EmnistSampler:
 
     def sample(self, char):
         #pdb.set_trace()
+        if char == ' ':
+            space_tensor = np.full((1, 28, 28), fill_value=-0.4242)
+            return torch.from_numpy(space_tensor)
+        if char.isupper():
+            char = char.lower()
         img_idx = random.randrange(0, len(self.letters[char]) - 1)
         return self.letters[char][img_idx]
 
-pdb.set_trace()
-
+#pdb.set_trace()
 sen_dataset = SentencesDataset("./text_generation/clean_text.txt")
-print("Example Sentence: ", sen_dataset[0])
+train_loader = torch.utils.data.DataLoader(sen_dataset[0][:], batch_size=32, shuffle=False)
+#pdb.set_trace()
+for i_batch, sample in enumerate(train_loader):
+    print("Epoch {}, Batch size {}\n".format(i_batch, 32))
+    if i_batch == 0:
+        print("Sentence is: {}".format(sample[1][:]))
+        for i in range(32):
+            plot(sample[0][i], letter_to_num(sample[1][i]))
+        pdb.set_trace()
 
-train_loader = torch.utils.data.DataLoader(sen_dataset, batch_size=32, shuffle=True)
-for i_batch, sample_batched in enumerate(train_loader):
-    pdb.set_trace()
+print("Done")
 
-
-
-# train_loader, test_loader = loaders.loader(batch_size_train=100, batch_size_test=1000)
 
 """ TODO
-- Test to see if space tensor is float32 and no dictionary key error anymore
-- Deal with Capital Leters either in the text cleaning or a function like to_lower()
-- Test to see if the list of tensors for each letter in my sentence is working properly
-- Test to see if the batching is working properly line 120. Print out the batches
-
 
 Question for Taylor
 - Added my own custom image for spaces
+- Format of the batch
 
 
 """
