1184319d200ae8a95fc2443060bd28e6c3d38a4e
Taylor <tahlor@gmail.com>
2021-10-25 18:49:08 -0600

    update message

--- lm/configs/slurm/gen.py
+++ lm/configs/slurm/gen.py
@@ -10,45 +10,50 @@ MEM = 100000
 
 class Generator():
     def __init__(self,
-                 group_path=Path("/lustre/scratch/grp/fslg_internn/"),
+                 group_path,
+                 proj_dir,
+                 python_script_path,
                  environment=None,
-                 proj_dir = None,
-                 cd_dir = None,
-                 python_script_dir = None,
-                 config_root = None,
-                 sh_proj_root = None,
-                 log_root = None,
+                 cd_dir=None,
+                 config_root=None,
+                 sh_proj_root=None,
+                 log_root=None,
                  hardware_dict=None,
                  *args, **kwargs):
         """
 
         Args:
-            group_path: FSLG Group directory
-            proj_dir: The main project folder -- EVERYTHING IS RELATIVE TO THIS
+            group_path: FSLG Group directory -- SHOULD MIRROR Github folder on local machine
+            proj_dir: The main project folder -- EVERYTHING IS RELATIVE TO THIS EXCEPT ENVIRONMENT
+            python_script_path: The path the python script is located (NO DEFAULT)
+            environment: RELATIVE TO GROUP_PATH
             cd_dir: The directory the python script should be run from (default: proj_dir)
-            python_script_dir: The directory the python script is located (default: cd_dir)
             config_root: The directory with the desired configs
-            sh_proj_root: Where to save the SH files (will mirror the config_root structure)
+            sh_proj_root: Where to save the SH files (will mirror the config_root structure); needs to be on GALOIS if running from GALOIS
             log_root: Where to save the logs (default: sh_proj_root)
             *args:
             **kwargs:
+            config_root_REFERENCE = WHERE TO REFERENCE CONFIGS ON FSL -- NEEDS TO BE FSL
+            config_root_SEARCH = WHERE TO SEARCH FOR CONFIGS -- NEEDS TO BE LOCAL (on GALOIS)
         """
 
         self.group_path = group_path
         self.env = self.group_path / "env/internn" if environment is None else environment
-        self.proj_dir = Path(self.group_path / "internn") if proj_dir is None else proj_dir
-        self.cd_dir = self.proj_dir if cd_dir is None else cd_dir
-        self.python_script_dir = self.python_script_dir if python_script_dir is None else python_script_dir
+        self.proj_dir = Path(self.group_path / "internn") if proj_dir is None else self.group_path / proj_dir
+        self.python_script_path = Path(self.proj_dir) / python_script_path
+        self.cd_dir = Path(self.python_script_path).parent if cd_dir is None else self.proj_dir / cd_dir
+
+        # Paths relative to project directory
+        self.config_root_REFERENCE = self.config_root_SEARCH = proj_dir / "configs" if config_root is None else self.proj_dir / config_root # the config root folder
+        self.sh_proj_root = self.cd_dir / "slurm/scripts" if sh_proj_root is None else sh_proj_root   # the .sh root folder
+        self.log_root = self.sh_proj_root if log_root is None else log_root
 
         # If running locally, setup path for files to be written to
         if socket.gethostname() in "Galois":
-            self.proj_dir = Path("/media/data/GitHub") / proj_dir.relative_to(self.group_path)
-            assert self.proj_dir.exists()
+            self.sh_proj_root = Path("/media/data/GitHub") / self.sh_proj_root.relative_to(self.group_path)
+            self.config_root_SEARCH = Path("/media/data/GitHub") / Path(self.config_root_REFERENCE).relative_to(self.group_path)
+            assert self.sh_proj_root
 
-        # Paths relative to project directory
-        self.config_root = proj_dir / "configs" if config_root is None else config_root # the config root folder
-        self.sh_proj_root = self.sh_proj_root / "slurm/scripts" if sh_proj_root is None else sh_proj_root   # the .sh root folder
-        self.log_root = self.sh_proj_root if log_root is None else log_root
 
         # Environment
 
@@ -56,13 +61,15 @@ class Generator():
             "default":{"threads":8, "time":"72:00:00", "gpu":"pascal"},
             } if hardware_dict is None else hardware_dict
 
-        self.loop_configs(group_path,
-                 environment,
-                 proj_dir,
-                 cd_dir,
-                 python_script_dir,
-                 config_root,
-                 sh_proj_root)
+        self.loop_configs(group_path=self.group_path,
+                          environment=self.env,
+                          proj_dir=self.proj_dir,
+                          cd_dir=self.cd_dir,
+                          python_script_path=self.python_script_path,
+                          config_root_SEARCH=self.config_root_SEARCH,
+                          config_root_REFERENCE=self.config_root_REFERENCE,
+                          sh_proj_root=self.sh_proj_root,
+                          log_root=self.log_root)
 
 
 
@@ -73,56 +80,57 @@ class Generator():
         mem = f"{int( MEM / threads )}MB"
         if cd_path is None:
             cd_path,_ = os.path.split(sh_path)
-        self.mkdir(sh_path, parent=True)
+        sh_path.parent.mkdir(exist_ok=True, parents=True)
         print(f"Writing {sh_path}")
         with open(f"{sh_path}", "w") as f:
                 f.write(f"""#!/bin/bash
-    #SBATCH --gres=gpu:1
-    #SBATCH -C 'rhel7&pascal'
-    #SBATCH --mem-per-cpu {mem}
-    #SBATCH --ntasks {threads}
-    #SBATCH --nodes=1
-    #SBATCH --output="{log_path}"
-    #SBATCH --time {time}
-    #SBATCH --mail-user={EMAIL}   # email address
-    #SBATCH --mail-type=BEGIN
-    #SBATCH --mail-type=END
-    #SBATCH --mail-type=FAIL
-    
-    #%Module
-    
-    module purge
-    module load cuda/10.1
-    module load cudnn/7.6
-    
-    export PATH="{env}:$PATH"
-    eval "$(conda shell.bash hook)"
-    conda activate $PATH
-    
-    cd "{cd_path}"
-    which python
-    {command}
+#SBATCH --gres=gpu:1
+#SBATCH -C 'rhel7&pascal'
+#SBATCH --mem-per-cpu {mem}
+#SBATCH --ntasks {threads}
+#SBATCH --nodes=1
+#SBATCH --output="{log_path}"
+#SBATCH --time {time}
+#SBATCH --mail-user={EMAIL}   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+#%Module
+
+module purge
+module load cuda/10.1
+module load cudnn/7.6
+
+export PATH="{env}:$PATH"
+eval "$(conda shell.bash hook)"
+conda activate $PATH
+
+cd "{cd_path}"
+which python
+{command}
     """)
 
     #def loop_configs(self, config_root, script_root, ext=".yaml"):
     def loop_configs(self,
-        group_path,
-        environment,
-        proj_dir,
-        cd_dir,
-        python_script_dir,
-        config_root,
-        sh_proj_root,
-        log_root):
+                     group_path,
+                     environment,
+                     proj_dir,
+                     cd_dir,
+                     python_script_path,
+                     config_root_SEARCH,
+                     config_root_REFERENCE,
+                     sh_proj_root,
+                     log_root):
 
-        p = Path(config_root)
+        p = Path(config_root_SEARCH)
         print(p)
         for config_path in p.rglob(f"*.yaml"):  # use rglob for recursion
             # Define paths
             subfolders = config_path.relative_to(p)  # = 'c/d'
             sh_path = Path(sh_proj_root / subfolders.parent / config_path.with_suffix('.sh').name)
             log_path = Path(log_root / subfolders.parent / ('log_' + config_path.with_suffix('.slurm').name))
-            py_script = python_script_dir / "lm/train_BERT.py"
+            py_script = python_script_path
             cd_path = cd_dir
             command = f"python -u {py_script} --config '{config_path}'"
             #print(f"sh:{sh_path} log:{log_path} cd: {cd_path}")
@@ -180,14 +188,14 @@ def delete_old_sh(path="."):
 if __name__=="__main__":
     delete_old_sh()
     Generator(group_path=Path("/lustre/scratch/grp/fslg_internn/"),
-             environment=None,
-             proj_dir=Path("./internn/lm"),
-             cd_dir=None,
-             python_script_dir=None,
-             config_root=".",
-             sh_proj_root=None,
-             log_root=None,
-             hardware_dict=None)
+              environment=None,
+              proj_dir=Path("./internn/lm"),
+              cd_dir=None,
+              python_script_path="train_BERT.py",
+              config_root="./configs",
+              sh_proj_root=None,
+              log_root=None,
+              hardware_dict=None)
 
     # Make scripts executable
     Popen('find . -type f -iname "*.sh" -exec chmod +x {}  \;', shell=True)
--- sync_to_fsl.sh
+++ sync_to_fsl.sh
@@ -2,7 +2,7 @@
 
 local_root="/media/data/GitHub/internn/"
 fsl_root="/home/taylor/shares/SuperComputerRoot/lustre/scratch/grp/fslg_internn/internn"
-rsync -rutv --update $local_root/data $fsl_root/data  --info=progress --exclude 'archive*'
+rsync -rutv --update $local_root/data $fsl_root  --info=progress --exclude 'archive*'
 
 # rsync -vurlt --size-only  '/home/pi/Downloads/external/' '/mnt/theserve/Media/Library/Audio Books/PodcastServer' --info=progress
 # v: verbose
